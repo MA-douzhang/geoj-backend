675d5e35c0f6e8b02b4a193becb97476a284cffd
MA_dou <841838856@qq.com>
2023-11-04 11:13:24 +0800

    仓库创建

---
+++ src/main/java/com/madou/geojbackendmicroservice/GeojBackendMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.madou.geojbackendmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeojBackendMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeojBackendMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madou/geojbackendmicroservice/GeojBackendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madou.geojbackendmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeojBackendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
