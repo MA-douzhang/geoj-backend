7cdce03e691580ad09cf17f361f0b099c8b0d278
MA_dou <841838856@qq.com>
2023-11-09 23:06:56 +0800

    fix 修复提交代码后没有返回的代码状态信息 11/9

--- geoj-code-sandbox/src/test/java/com/madou/geojcodesandbox/GeojCodeSandboxApplicationTests.java
+++ geoj-code-sandbox/src/test/java/com/madou/geojcodesandbox/GeojCodeSandboxApplicationTests.java
@@ -8,6 +8,7 @@ class GeojCodeSandboxApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ geoj-code-sandbox/src/test/java/com/madou/geojcodesandbox/test.java
@@ -0,0 +1,18 @@
+package com.madou.geojcodesandbox;
+
+/**
+ * @author MA_dou
+ * @version 1.0
+ * @project geoj-backend-microservice
+ * @description
+ * @date 2023/11/5 22:33:03
+ */
+import java.util.Scanner;
+public class test{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
